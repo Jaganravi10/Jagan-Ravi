3b1b4dcae89e0b36af7ce58d347e104599ad8f15
Jaganravi10 <118062218+Jaganravi10@users.noreply.github.com>
2022-11-12 20:22:18 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,7 @@
+
+public class Class1 {
+	int x = 10;
+	char [] y = {'A','B','C'};
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+    
+		Class1 d = new Class1();
+		System.out.println(d.y);
+
+	}
+
+}
