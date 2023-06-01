a2cdb6a840762e2c37f35db55b55ac19e88d410c
Jagan_Ravi <118062218+Jaganravi10@users.noreply.github.com>
2023-06-01 10:14:07 +0530

    Update ArrayContainsElementOrNot.java
    With two methods

--- ArrayContainsElementOrNot.java
+++ ArrayContainsElementOrNot.java
@@ -2,26 +2,45 @@ import java.util.stream.IntStream;
 
 public class ArrayContainsElementOrNot {
 	
-	int[] array = {1,2,4,5,6,7};
+	int [] given = {1,2,3,4,5,6,6,7,8,9};
 	
-	int numberToFind=13;
+	int numberToBeFound= 6;
 	
-	public void usingIntStream() {
-		
-	boolean found=IntStream.of(array).anyMatch(element->element==numberToFind);
-		
-	if(found) {
-		System.out.println("Element present");
-		
-	}else
-		System.out.println("Element not present");
+	boolean found=false;
 	
+	public void containsElementOrNot() {
+		
+		for (int number : given) {
+			if(number == numberToBeFound) {
+				found=true;
+				System.out.println("Number found is "+ numberToBeFound );
+				break;
+			}
+		}
+		
+		if(found) {
+			System.out.println("Number found");
+		}else {
+			System.out.println("Number not found");
+		}
 	}
 
+	public void usingIntStream() {
+		
+		boolean found=IntStream.of(given).anyMatch(element->element==numberToBeFound);
+		
+		if(found)
+			System.out.println("Number found");
+		else
+			System.out.println("Not found");		
+	}
+	
 	public static void main(String[] args) {
 
 		ArrayContainsElementOrNot arrayContainsElementOrNot = new ArrayContainsElementOrNot();
+//		arrayContainsElementOrNot.containsElementOrNot();
 		arrayContainsElementOrNot.usingIntStream();
+
 	}
 
 }
