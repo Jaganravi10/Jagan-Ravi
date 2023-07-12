ef310428157fb1e54886542e10b859a1a93d9713
Jagan_Ravi <118062218+Jaganravi10@users.noreply.github.com>
2023-07-12 20:32:18 +0530

    Find Ascii Value

---
+++ FindAscii.java
@@ -0,0 +1,14 @@
+
+public class FindAscii {
+
+	public static void main(String[] args) {
+		
+		char c = 'z';
+		int ascii = c;
+		
+		int ascciNumber = (int)c;
+		
+		System.out.println(ascciNumber);
+	}
+
+}
