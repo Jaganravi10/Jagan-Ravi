63d747bae0e55dd25f06a4f1b2f407d1bdfe1a08
Jagan_Ravi <118062218+Jaganravi10@users.noreply.github.com>
2024-04-26 07:52:53 +0530

    Updated commit

--- FindAscii.java
+++ FindAscii.java
@@ -1,14 +1,26 @@
 
 public class FindAscii {
+	
+	public static void findAscciValue() {
+		
+		char f = 'f';
+		int value = (int)f;
+		System.out.println("Ascii value of " + f + " is " + value );
+	}
+	
+	public static void findAscciValueOfString() {
+		
+		String givenValue = "God";
+		
+		for(int i=0; i<givenValue.length(); i++) {
+			int asciiValue = givenValue.charAt(i);
+			System.out.println(asciiValue);
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		char c = 'z';
-		int ascii = c;
-		
-		int ascciNumber = (int)c;
-		
-		System.out.println(ascciNumber);
-	}
+		findAscciValueOfString();
 
+	}
 }
